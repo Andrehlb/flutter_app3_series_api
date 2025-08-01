b0bcd967a2b40bc17f78dccfe72ef0a4fcaf1174
Andrehlb <andreh.72@hotmail.com>
2025-08-01 18:34:42 -0300

    feat: add basic layout of TvShowScreen with Scaffold and placeholder text
    feat: 添加带有 Scaffold 和占位文本的基本 TvShowScreen 布局

---
+++ lib/tv_show_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app3_series_api/tv_show_model.dart';
+
+class TvShowScreen extends StatelessWidget {
+  const TvShowScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Séries'),
+      ),
+    );
+  }
+}
