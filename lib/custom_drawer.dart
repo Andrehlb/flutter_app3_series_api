4c0a171938879f2f749cef2ef1579d7752fd0062
Andrehlb <andreh.72@hotmail.com>
2025-08-03 08:04:02 -0300

    add CustomDrawer with sidebar menu (TV Shows and Favorites)

---
+++ lib/custom_drawer.dart
@@ -0,0 +1,37 @@
+import 'package: flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class CustomDrwawer extends StatelessWidget {
+  const CustomDrwawer({super,key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: const [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+            ),
+            child: Text(
+              'Eu Amo Séries',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ), 
+          ),
+          ListTile(
+            leading: Icon(Icons.tv),
+            title: Text('Séries'),
+          ),
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text('Favoritas'),
+          ),
+        ],
+      ),
+    );
+  } // Widget build
+} // class CustomDrwawer
