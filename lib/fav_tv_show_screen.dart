d670bb0a688f0a0e2bc4dfc1716d801dd091079f
Andrehlb <andreh.72@hotmail.com>
2025-08-03 23:13:30 -0300

    feat: implement FavTvShowScreen for displaying favorite TV shows

---
+++ lib/fav_tv_show_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'tv_show_model.dart';
+
+class FavTvShowScreen extends StatefullWidget {
+  const FavTvShowScreen({super.key});
+
+  @override
+  _FavTvShowScreenState createState() => _FavTvShowScreenState();
+}
