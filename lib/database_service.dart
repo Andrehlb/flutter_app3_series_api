04920758b7d82d31be3f7965a5c381fb1db943f0
Andrehlb <andreh.72@hotmail.com>
2025-08-09 19:35:00 -0300

    feat: initialize DatabaseService with database name and version constants

--- lib/database_service.dart
+++ lib/database_service.dart
@@ -1,2 +1,8 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+
+class DatabaseService {
+  static const _databaseName = 'tv_shows_database.db';
+  static const _databaseVersion = 1;
+
+  static Database? _database;
