38cdf410912ebad60f4f6ab1e7b3335298799fa8
Andrehlb <andreh.72@hotmail.com>
2025-08-09 22:04:11 -0300

    feat: implement TvShowSearchScreen with initial setup

--- lib/tv_show_search.dart
+++ lib/tv_show_search.dart
@@ -3,3 +3,10 @@ import 'package:app3_series_api/tv_show_model.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+
+class TvShowSearchScreen extends StatefulWidget {
+  const TvShowSearchScreen({super.key});
+
+  @override
+  State<TvShowSearchScreen> createState() => _TvShowSearchScreenState();
+}
