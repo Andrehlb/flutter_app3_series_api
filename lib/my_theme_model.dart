d85fd32410a558eb6d8553d620a785a7db2f2e7f
Andrehlb <andreh.72@hotmail.com>
2025-08-09 21:43:17 -0300

    feat: implement MyThemeModel for theme management

--- lib/my_theme_model.dart
+++ lib/my_theme_model.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+class MyThemeModel extends ChangeNotifier {
+
+  bool isDark = true;
+  MyTheme myTheme = MyTheme(color: Color(0xff8716d5));
+  ThemeData get customTheme => myTheme.customTheme;
+  ThemeData get customThemeDark => myTheme.customThemeDark;
+  ThemeMode get themeMode => isDark ? ThemeMode.dark : ThemeMode.light;
+
+  void toogleTheme() {
+    isDark = !isDark;
+    notifyListeners();
+  }
+}
