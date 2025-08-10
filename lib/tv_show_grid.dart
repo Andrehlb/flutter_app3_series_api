23a6aab94778385226ff22df1d90f3be1dacbcfa
Andrehlb <andreh.72@hotmail.com>
2025-08-09 21:54:12 -0300

    feat: add TvShowGrid widget with initial implementation

--- lib/tv_show_grid.dart
+++ lib/tv_show_grid.dart
@@ -2,3 +2,12 @@ import 'package:app3_series_api/tv_show_model.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+
+class TvShowGrid extends StatefulWidget {
+  const TvShowGrid({super.key, required this.tvShows});
+
+  final List<TvShow> tvShows;
+
+  @override
+  State<TvShowGrid> createState() => _TvShowGridState();
+}
